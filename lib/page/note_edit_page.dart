9ce51d3d4e6382bcd5545a62e2a2ab29beeee20c
westhyun <pkwesst@gmail.com>
2023-05-17 19:41:34 +0900

    ⚒️fix : 입력 부분 외의 배경색도 변경될 수 있도록 수정

--- lib/page/note_edit_page.dart
+++ lib/page/note_edit_page.dart
@@ -33,40 +33,42 @@ class _NoteEditPageState extends State<NoteEditPage> {
             ), // 노트 색상 변경 버튼
           ],
         ),
-        body: Container(
-          color: color, // 노트 배경 색상
-          child: SingleChildScrollView(
-            // 노트 편집 시 화면을 넘어가는 경우 스크롤
-            padding: EdgeInsets.symmetric(horizontal: 12.0, vertical: 16.0),
-            // 제목, 본문 margin가
-            child: Column(
-              // 제목과 본문은 수직방향
-              mainAxisSize: MainAxisSize.min, // 본문은 작성한 내용만큼만 차지
-              children: [
-                // 입력 필드
-                TextField(
-                  // text 형식을 user에게 입력받음
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(), // 제목 필드 border
-                    labelText: '제목 입력,', // 제목
+        body: SizedBox.expand( // text 입력 부분 외의 배경색도 바뀌도록 추가
+          child: Container(
+            color: color, // 노트 배경 색상
+            child: SingleChildScrollView(
+              // 노트 편집 시 화면을 넘어가는 경우 스크롤
+              padding: EdgeInsets.symmetric(horizontal: 12.0, vertical: 16.0),
+              // 제목, 본문 margin가
+              child: Column(
+                // 제목과 본문은 수직방향
+                mainAxisSize: MainAxisSize.min, // 본문은 작성한 내용만큼만 차지
+                children: [
+                  // 입력 필드
+                  TextField(
+                    // text 형식을 user에게 입력받음
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(), // 제목 필드 border
+                      labelText: '제목 입력,', // 제목
+                    ),
+                    maxLines: 1, // 제목 표시할 line 수
+                    style: TextStyle(fontSize: 20.0), // 제목 크기
+                    controller: titleController, // 제목 text 생성, 편집 위해 controller
                   ),
-                  maxLines: 1, // 제목 표시할 line 수
-                  style: TextStyle(fontSize: 20.0), // 제목 크기
-                  controller: titleController, // 제목 text 생성, 편집 위해 controller
-                ),
-                SizedBox(
-                  height: 8.0,
-                ), // 제목-본문 사이 공백
-                TextField(
-                  decoration: InputDecoration(
-                    border: InputBorder.none, // 본문 border none
-                    hintText: '노트 입력', // 노트 본문 입력 안내 text
-                  ),
-                  maxLines: null, // 본문 내용만큼 늘어날 수 있도록
-                  keyboardType: TextInputType.multiline, // 개행
-                  controller: bodyController, // 본문 text controller
-                )
-              ],
+                  SizedBox(
+                    height: 8.0,
+                  ), // 제목-본문 사이 공백
+                  TextField(
+                    decoration: InputDecoration(
+                      border: InputBorder.none, // 본문 border none
+                      hintText: '노트 입력', // 노트 본문 입력 안내 text
+                    ),
+                    maxLines: null, // 본문 내용만큼 늘어날 수 있도록
+                    keyboardType: TextInputType.multiline, // 개행
+                    controller: bodyController, // 본문 text controller
+                  )
+                ],
+              ),
             ),
           ),
         ));
